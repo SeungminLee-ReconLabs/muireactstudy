2bbbb5b039a96e183ac5d3382cb494a102bd17c5
Seungmin Lee <seungmin.lee@reconlabs.kr>
2021-10-08 17:45:36 +0900

    themp

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import { ThemeProvider } from "@material-ui/styles";
 import { Route, Switch } from "react-router-dom";
-import {getCookie, setCookie} from "./lib/webAPIs/cookie"
 import { LogIn, Register, Product } from "./pages";
-import { Admin, Resource, ListGuesser } from 'react-admin';
+// import { Admin, Resource, ListGuesser } from 'react-admin';
+import { ListGuesser } from 'react-admin';
 import jsonServerProvider from 'ra-data-json-server';
 import CustomRoutes from "./CustomRoutes";
-import { sideBarWidth } from "./cssVariables";
-// import { layout } from "./CustomLayout";
 import AllCustomLayout from "./AllCustomLayout";
 import { createTheme } from '@material-ui/core/styles'
 import { CssBaseline } from "@material-ui/core";
++import PropTypes from "prop-types";
++import { ConnectedRouter } from 'connected-react-router';
++import { Switch, Route } from 'react-router-dom';
++import withContext from 'recompose/withContext';
++import { AuthContext, DataProviderContext, TranslationProvider, Resource, Notification } from 'react-admin';
++import AppBar from '@material-ui/core/AppBar';
++import Toolbar from '@material-ui/core/Toolbar';
++import Typography from '@material-ui/core/Typography';
+
 
 /**
  * 최상단에 위치한 App.js에서 MUI(material ui)의 커스텀테마를 생성합니다
@@ -41,10 +48,13 @@ const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
 function App() {
   return (
     <>
-    <CssBaseline>
-    <ThemeProvider theme={theme}>
++       <AuthContext.Provider value={authProvider}>
++       <DataProviderContext.Provider value={dataProvider}></DataProviderContext.Provider>  
+      <CssBaseline>
+      <ThemeProvider theme={theme}>
 
-        <Admin
+        {/* Admin 사용 코드 */}
+        {/* <Admin
           customRoutes={CustomRoutes}
           // theme={theme}
           // layout={layout}
@@ -55,9 +65,38 @@ function App() {
           <Resource name="posts" list={ListGuesser} options={{label: "PlicAR : 쉽고 빠른 AR 커머스"}}/>
           <Resource name="users" list={ListGuesser} />
           <Resource name="library" list={ListGuesser} />
-        </Admin>
-    </ThemeProvider>
-    </CssBaseline>
+        </Admin> */}
++               <AppBar position="static" color="default">
++                   <Toolbar>
++                       <Typography variant="h6" color="inherit">
++                           My admin
++                       </Typography>
++                   </Toolbar>
++               </AppBar>
++               <ConnectedRouter history={history}>
++                   <Switch>
++                       <Route exact path="/" component={Dashboard} />
++                       <Route exact path="/posts" render={(routeProps) => <PostList hasCreate resource="posts" basePath={routeProps.match.url} {...routeProps} />} />
++                       <Route exact path="/posts/create" render={(routeProps) => <PostCreate resource="posts" basePath={routeProps.match.url} {...routeProps} />} />
++                       <Route exact path="/posts/:id" render={(routeProps) => <PostEdit hasShow resource="posts" basePath={routeProps.match.url} id={decodeURIComponent((routeProps.match).params.id)} {...routeProps} />} />
++                       <Route exact path="/posts/:id/show" render={(routeProps) => <PostShow hasEdit resource="posts" basePath={routeProps.match.url} id={decodeURIComponent((routeProps.match).params.id)} {...routeProps} />} />
++                       <Route exact path="/comments" render={(routeProps) => <CommentList hasCreate resource="comments" basePath={routeProps.match.url} {...routeProps} />} />
++                       <Route exact path="/comments/create" render={(routeProps) => <CommentCreate resource="comments" basePath={routeProps.match.url} {...routeProps} />} />
++                       <Route exact path="/comments/:id" render={(routeProps) => <CommentEdit resource="comments" basePath={routeProps.match.url} id={decodeURIComponent((routeProps.match).params.id)} {...routeProps} />} />
++                       <Route exact path="/users" render={(routeProps) => <UsersList hasCreate resource="users" basePath={routeProps.match.url} {...routeProps} />} />
++                       <Route exact path="/users/create" render={(routeProps) => <UsersCreate resource="users" basePath={routeProps.match.url} {...routeProps} />} />
++                       <Route exact path="/users/:id" render={(routeProps) => <UsersEdit resource="users" basePath={routeProps.match.url} id={decodeURIComponent((routeProps.match).params.id)} {...routeProps} />} />
++                   </Switch>
++               </ConnectedRouter>
++               <Notification />
+
+
+      </ThemeProvider>
+      </CssBaseline>
+
++       </DataProviderContext.Provider>
++       </AuthContext.Provider>
+
 
     </>
   );
@@ -67,4 +106,11 @@ function App() {
 // dev https://plm-dev.aseeo.io:3003/plm#
 // production https://plm.aseeo.io:3003/plm
 
-export default App;
+// export default App;
+
++export default withContext(
+  +   {
+  +       authProvider: PropTypes.object,
+  +   },
+  +   () => ({ authProvider })
+  +)(App);
